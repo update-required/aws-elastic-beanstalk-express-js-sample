33015b37209b90019c87882335f1c44fd3044b3b
David  Aaku <david@Davids-MacBook-Pro.local>
2024-10-01 21:35:04 -0400

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('David is the Best AWS DevOps agent there is'));
+app.get('/', (req, res) => res.send('David is the Best AWS DevOps agent there is - this commit is different'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
